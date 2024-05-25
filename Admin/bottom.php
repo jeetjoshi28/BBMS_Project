b230c9e34660509de3debe190b7b3c6481873f54
Joshi Jeet K <jeetjoshi@yahoo.com>
2024-05-25 18:36:28 +0530

    uplode project

---
+++ Admin/bottom.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+ <div class="clear"></div>
+<div class="ftr-bg">
+<div class="wrap">
+<div class="footer">
+	<div class="f_nav">
+		<ul>
+			
+	    </ul>
+	</div>
+	<div style="color:#805752;">
+	<fotter>
+		<center>
+			<p> ©2024 All Rights Reserved | Design by: Mr.Jeet k Joshi | From: Manipal University Jaipur</p>			
+	</center>
+</footer>
+	<div class="clear"></div>
+</div>
+</div>
+</div>
+</body>
+</html>
